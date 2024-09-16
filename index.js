9b9b8c4a1c11703063ee22f86cedf340a1697ca6
sadelcri <sadelcry@gmail.com>
2024-09-16 12:46:33 +0200

    added files to the repo

--- index.js
+++ index.js
@@ -1,89 +1,85 @@
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    let choice;
-    let randomNumber = Math.random() * 3;
-
-    if (randomNumber < 1) {
-        choice = rock;
-    } else if (randomNumber >= 1 && randomNumber < 2) {
-        choice = paper;
-    } else {
-        choice = scissors;
-    }
-
-    return choice;
+  let choice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      choice = rock;
+      break;
+    case 1:
+      choice = paper;
+      break;
+    case 2:
+      choice = scissors;
+      break;
+  }
+  return choice;
 }
 
 function getHumanChoice() {
-    let choice = prompt("Choose: rock, paper or scissors");
-    return choice.toLowerCase();
+  let choice = prompt("Elige piedra, papel o tijeras:");
+  switch (choice.toLowerCase()) {
+    case "piedra":
+      choice = rock;
+      break;
+    case "papel":
+      choice = paper;
+      break;
+    case "tijeras":
+      choice = scissors;
+      break;
+  }
+  return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === rock && computerChoice === scissors) {
+    humanScore++;
+    console.log("¡Ganas esta ronda, genio! 🏆");
+  } else if (humanChoice === rock && computerChoice === paper) {
+    computerScore++;
+    console.log("Pierdes, pringado. 💩");
+  } else if (humanChoice === paper && computerChoice === rock) {
+    humanScore++;
+    console.log("¡Ganas esta ronda, crack! 🏆");
+  } else if (humanChoice === paper && computerChoice === scissors) {
+    computerScore++;
+    console.log("Te han cortado con las tijeras. Pierdes. 💩");
+  } else if (humanChoice === scissors && computerChoice === paper) {
+    humanScore++;
+    console.log("¡Cortas el papel! Ganas la ronda. 🏆");
+  } else if (humanChoice === scissors && computerChoice === rock) {
+    computerScore++;
+    console.log("La roca te aplasta, palurdo. Pierdes. 💩");
+  } else if (humanChoice === computerChoice) {
+    console.log("Empate");
+  }
 }
 
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === rock && computerChoice === scissors) {
-            console.log("You win! rock beats scissors");
-            humanScore = humanScore + 1;
-        } else if (humanChoice === rock && computerChoice === paper) {
-            console.log("You lose! paper beats rock");
-            computerScore = computerScore + 1;
-        } else if (humanChoice === rock && computerChoice === rock) {
-            console.log("It's a draw!");
-        } else if (humanChoice === paper && computerChoice === rock) {
-            console.log("You win! paper beats rock");
-            humanScore = humanScore + 1;
-        } else if (humanChoice === paper && computerChoice === scissors) {
-            console.log("You lose! scissors beats paper");
-            computerScore = computerScore + 1;
-        } else if (humanChoice === paper && computerChoice === paper) {
-            console.log("It's a draw!");
-        } else if (humanChoice === scissors && computerChoice === rock) {
-            console.log("You lose! rock beats scissors");
-            computerScore = computerScore + 1;
-        } else if (humanChoice === scissors && computerChoice === paper) {
-            console.log("You win! scissors beats paper");
-            humanScore = humanScore + 1;
-        } else if (humanChoice === scissors && computerChoice === scissors) {
-            console.log("It's a draw!");
-        }
-    }
-
-    // Llamadas a playRound cinco veces
-    let humanChoice1 = getHumanChoice();
-    let computerChoice1 = getComputerChoice();
-    playRound(humanChoice1, computerChoice1);
-
-    let humanChoice2 = getHumanChoice();
-    let computerChoice2 = getComputerChoice();
-    playRound(humanChoice2, computerChoice2);
-
-    let humanChoice3 = getHumanChoice();
-    let computerChoice3 = getComputerChoice();
-    playRound(humanChoice3, computerChoice3);
-
-    let humanChoice4 = getHumanChoice();
-    let computerChoice4 = getComputerChoice();
-    playRound(humanChoice4, computerChoice4);
-
-    let humanChoice5 = getHumanChoice();
-    let computerChoice5 = getComputerChoice();
-    playRound(humanChoice5, computerChoice5);
-
-    console.log(`Final Scores - Human: ${humanScore}, Computer: ${computerScore}`);
-
-    if (humanScore > computerScore) {
-        console.log("You win the game!");
-    } else if (computerScore > humanScore) {
-        console.log("Computer wins the game!");
-    } else {
-        console.log("The game is a draw!");
-    }
+  if (humanScore > computerScore) {
+    console.log(
+      "¡Has ganado!, habeis quedado: " + humanScore + "," + computerScore
+    );
+  } else if (humanScore == computerScore) {
+    console.log(
+      "¡Empate!, habeis quedado: " + humanScore + "," + computerScore
+    );
+  } else {
+    console.log(
+      "¡Has perdido!, habeis quedado: " + humanScore + "," + computerScore
+    );
+  }
 }
 
 playGame();
